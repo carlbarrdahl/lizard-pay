27bac514bb7c6080b2567d1e5705dd8609a35c1e
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-08-24 09:55:57 +0200

    Add InvoicesTable

---
+++ features/dashboard/components/InvoicesTable.tsx
@@ -0,0 +1,13 @@
+import { Table, Th, Thead, Tr } from "@chakra-ui/react";
+
+export default function InvoicesTable() {
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th></Th>
+        </Tr>
+      </Thead>
+    </Table>
+  );
+}
